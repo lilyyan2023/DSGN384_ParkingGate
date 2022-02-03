1343a704bc4421c1d4580224e51463de6e575fa9
Ross Wojcik <rosswojcik@dhcp-10-105-28-158.wireless.northwestern.private>
2022-02-02 23:28:12 -0600

    Mockup_2 update with gate motor and day/night functionality

--- mockup_2.py
+++ mockup_2.py
@@ -1,7 +1,3 @@
-
-# Entry Point for Serial Output
-print("version32")
-
 # Library and Package Imports
 import time
 import board
@@ -9,7 +5,14 @@ import busio
 from digitalio import DigitalInOut, Direction, Pull
 import analogio
 import adafruit_mprls
+import pwmio
+from adafruit_motor import servo
 
+# Entry Point for Serial Output
+print("version32")
+
+
+### Pin Initialization ################################################################
 # Startup LED Initialization
 LED1 = DigitalInOut(board.D10)
 LED1.direction = Direction.OUTPUT
@@ -26,44 +29,88 @@ light_sensor = analogio.AnalogIn(board.A5)
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
 
-# Global Variables
-dt = 0.1
-latched = False
+# Create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+
+### Global Variables ##################################################################
+dt = 0.1  # timestep
+latched = False  # pressure sensor latch
+daytime = False  # light sensor latch
 latch_timer = 0
 count = 0
+time_0 = time.monotonic()  # initial time
 
-# Data Variables
-cars = []
 
+### Data Variables ####################################################################
+car_data = []
+time_data = []
+pressure_data = []
+light_data = []
+
+
+### Helper Functions ##################################################################
 def getTime(local_time):
 
     t = local_time
-    timestamp = timestamp = "%d/%d/%d %d:%d:%d" % (t.tm_mon,t.tm_mday,t.tm_year,t.tm_hour,t.tm_min,t.tm_sec)
+    timestamp = timestamp = "%d/%d/%d %d:%d:%d" % (t.tm_mon, t.tm_mday, t.tm_year, t.tm_hour, t.tm_min, t.tm_sec)
 
     return timestamp
 
 
-# Startup Light Sequence
+### Startup Light Sequence ############################################################
 for i in range(3):
     LED1.value = True
     time.sleep(0.2)
     LED1.value = False
     time.sleep(0.2)
 
-# Collect Data Until Stopped
+for angle in range(50, 180, 2):
+    my_servo.angle = angle
+    time.sleep(0.02)
+
+for angle in range(180, 50, -2):
+    my_servo.angle = angle
+    time.sleep(0.02)
+
+
+### Sensor Loop #######################################################################
+
 while True:
 
-    # Pushbutton Stop
+    ### Pushbutton Stop ###
     if PB1.value < 2000:
         break
 
-    # Pressure Sensor Readout
-    print("Pressure Sensor")
-    print((mpr.pressure,))
+    ### Sensor Data Collection ###
+    time_data.append(time.monotonic() - time_0)  # time
+    pressure_data.append(mpr.pressure)         # pressure
+    light_data.append(light_sensor.value)      # light
 
-    # Light Sensor Readout
-    print("Light Sensor:")
+    ### Sensor Readouts ###
+    #print("Pressure Sensor")
+    #print((mpr.pressure,))
+    #print("Light Sensor:")
     #print((light_sensor.value,))
+
+    # Open the Gate at Dawn
+    if (not daytime) and light_sensor.value > 500:
+
+        for angle in range(50, 180, 2):
+            my_servo.angle = angle
+            time.sleep(0.02)
+
+        daytime = True
+
+    # Close the Gate at Dusk
+    if daytime and light_sensor.value < 100:
+        for angle in range(180, 50, -2):
+            my_servo.angle = angle
+            time.sleep(0.02)
+
+        daytime = False
 
     # Pressure Sensor Counting and Latch
     if (not latched):
@@ -73,7 +120,7 @@ while True:
             Pulse.value = 1
             Pulse.value = 0
             latched = True
-            cars.append((count, getTime(time.localtime())))
+            car_data.append((count, getTime(time.localtime())))
 
     if latched:
         latch_timer += 1
@@ -82,9 +129,13 @@ while True:
         latched = False
         latch_timer = 0
 
+    # Timestep
     time.sleep(dt)
 
-print(cars)
+print(time_data)
+print(pressure_data)
+print(light_data)
+print(car_data)
 
 
 
